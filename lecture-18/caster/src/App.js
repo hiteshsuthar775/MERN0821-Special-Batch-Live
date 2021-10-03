9e9811f54ba838bd7a3c434f1ee1199623170a92
Hiteshsuthar <HIteshsuthar775@gmail.com>
2021-10-03 19:52:10 +0530

    whatapp

--- lecture-18/caster/src/App.js
+++ lecture-18/caster/src/App.js
@@ -1,11 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
+import send from "./send.svg";
+import "./App.css"; 
+import Hi from "./Hit.jpg"
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
+import lo from "./lo.gif";
 
 function App() {
   let [socket, setSocket] = useState();
   let [message, setMessage] = useState("");
+
   let [chats, setChats] = useState([]);
 
   useEffect(function () {
@@ -14,72 +17,59 @@ function App() {
     soc.on("connect", () => {
       setSocket(soc);
     });
-
+   
     soc.on("broadcast", (payload) => {
-      
-      setChats(function(old){
-         let copy = [...old];
-         copy.push(payload);
-         return copy;
-      })
-
+      setChats(function (old) {
+        let copy = [...old];
+        copy.push(payload);
+        return copy;
+      });
     });
-
   }, []);
 
   if (socket) {
     return (
-      <div
-        style={{
-          width: "100vw",
-          height: "100vh",
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        <div
-          style={{
-            width: "100vw",
-            display: "flex",
-            flexDirection: "column",
-            flexGrow: 1,
-          }}
-        >
-          {chats.map(function(chat, index){
-            return <p>{chat.message}</p>
-          })}
+      <div className="hit1"><p className="heding">Hiteshsuthar</p>
+      <p className="heding1">Online</p>
+      <img className="img1" src={Hi} alt="" srcset="" />
+      
+        <div className="style">
+          <div className="style1">
+            {chats.map(function (chat, index) {
+              return <p className="MASS">{chat.message} <span id="demo"></span></p>;
+            })}
+
+          </div>
         </div>
 
-        <div
-          style={{
-            margin: "20px",
-            height: "60px",
-            display: "flex",
-            flexDirection: "row",
-          }}
-        >
-          <input
-            value={message}
-            style={{
-              flexGrow: 1,
-            }}
-            onChange={function (event) {
-              setMessage(event.target.value);
-            }}
-          />
-          <button
-            onClick={function () {
-              socket.emit("message", { message: message });
-              setMessage("");
-            }}
-          >
-            Send
-          </button>
+        <div className="hit">
+          <div className="style3">
+            {" "}
+           
+            <input
+              placeholder="Type a massages"
+              value={message}
+              className="input"
+              onChange={function (event) {
+                setMessage(event.target.value);
+              }}
+            />
+            <button
+              className="button"
+              onClick={function () {
+                socket.emit("message", { message: message });
+                setMessage("");
+              }}
+            >
+              <img className="Helo" src={send} alt="helo" srcset="" />
+            </button>
+          </div>
         </div>
       </div>
     );
   } else {
-    return <h1> Connecting .....</h1>;
+   
+    return (<div><center><img className="Helo2" src={lo} alt="helo" srcset=""/><h1  className="hed">Connecting.....</h1></center></div>);
   }
 }
 
